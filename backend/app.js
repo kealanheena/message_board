778b2c12b7f08d9bbfbe54c8e66896484c5c123b
kealanheena <kealanheena@gmail.com>
2020-05-08 07:52:14 +0100

    Chore: add post function

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,16 @@ class MessageApp {
   constructor() {
     this.messages = []
   }
+
+  post (content) {
+    let item = {
+      id: this.messages.length,
+      content: content,
+      date: new Date()
+    }
+    this.messages.push(item)
+    return this.message
+  };
 }
 
 export default MessageApp
--- backend/app.test.js
+++ backend/app.test.js
@@ -2,8 +2,19 @@ import { expect } from "chai";
 import MessageApp from "./app";
 
 describe("app", () => {
-  let testApp = new MessageApp
+
+  let testApp
+  beforeEach(() => {
+    testApp = new MessageApp
+    testApp.post('hi world')
+  })
+
   it("app has messages", () => {
     expect(testApp.messages).to.be.an("array");
+  });
+
+  it("app creates message (post)", () => {
+    testApp.post('hi world')
+    expect(testApp.messages.length).to.equal(2)
   });
 });
