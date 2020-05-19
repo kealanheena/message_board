d92411838d09c0a5684228b6e5abf8f54a7ff470
kealanheena <kealanheena@gmail.com>
2020-05-19 22:17:45 +0100

    Chore: complete backend fuctionality

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app = express()
 const PORT = 3000
@@ -7,6 +8,7 @@ const PORT = 3000
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use(require("./lib/routes.js"))
 
