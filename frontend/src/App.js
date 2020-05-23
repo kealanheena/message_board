44095c40acd1264995f11901946b4575d4dc64b7
kealanheena <kealanheena@gmail.com>
2020-05-23 23:30:46 +0100

    Chore: write first front end test

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function MessageApp() {
+  return (
+    <div className="App">
+      <textarea id="message_box"></textarea>
+      <br/>
+      <button
+      type="submit"
+      name="Submit"
+      id="submit">
+        Submit
+      </button>
+      <ul
+      id="message_list">
+        message
+      </ul>
+    </div>
+  );
+}
+
+export default MessageApp;
