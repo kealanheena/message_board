a878df613907734927ac260cadce468021e79e56
kealanheena <kealanheena@gmail.com>
2020-06-02 22:06:34 +0100

    Chore: add mocks and make messageList tests use them

--- frontend/src/components/messageList/messageList.test.js
+++ frontend/src/components/messageList/messageList.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MessageList from './messageList';
+import mockMessages from '../../__mocks__/messages.json';
 
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -15,14 +16,14 @@ describe('List', () => {
 
   it('takes messages as props and displays them', () => {
     const component = shallow(<MessageList
-      messages={[{id: 1, content: 'hello', date: '2000'}, {id: 2, content: 'world', date: '2001'}]}
+      messages={mockMessages}
     />);
-    expect(component.find('ul#message_list').children().length).toBe(2);
+    expect(component.find('ul#message_list').length).toBe(1);
   })
 
   it('each message in list has a delete button', () => {
     const component = mount(<MessageList 
-      messages={[{id: 1, content: 'hello', date: '2000'}]}
+      messages={mockMessages}
     />);
     expect(component.find('ul#message_list').childAt(0).exists('button#delete')).toBe(true);
   })
